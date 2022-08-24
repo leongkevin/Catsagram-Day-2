c5eae17a73f4446b90d1015fa5e90e681e6aedf7
Kevin Leong <leongkevin@outlook.com>
2022-08-23 20:03:27 -0400

    Upvote downvote button

--- public/main.js
+++ public/main.js
@@ -1,35 +1,50 @@
 export const createMainContent = () => {
-    // Create h1
-    const h1 = document.createElement("h1");
-    h1.innerText = "Catstagram";
+	// Create h1
+	const h1 = document.createElement('h1');
+	h1.innerText = 'Catstagram';
 
-    // Create img
-    const img = document.createElement("img");
-    img.style.margin = "20px";
-    img.style.maxWidth = "750px";
+	// Create img
+	const img = document.createElement('img');
+	img.style.margin = '20px';
+	img.style.maxWidth = '750px';
 
-    const container = document.querySelector(".container");
-    container.appendChild(h1);
-    container.appendChild(img);
+	const container = document.querySelector('.container');
+	container.appendChild(h1);
+	container.appendChild(img);
 
-    const requestButton = document.createElement("button");
-    requestButton.innerHTML = "REQUEST"
-    container.appendChild(requestButton);
+    const buttonContainer = document.createElement('div');
+    container.appendChild(buttonContainer);
 
+	const requestButton = document.createElement('button');
+	requestButton.innerHTML = 'REQUEST';
+	buttonContainer.appendChild(requestButton);
+	requestButton.addEventListener('click', (e) => {
+		fetchImage();
+	});
 
-    fetchImage();
+	const upvoteButton = document.createElement('button');
+	upvoteButton.innerHTML = 'UPVOTE';
+	buttonContainer.appendChild(upvoteButton);
+
+	const downvoteButton = document.createElement('button');
+	downvoteButton.innerHTML = 'DOWNVOTE';
+	buttonContainer.appendChild(downvoteButton);
+
+	fetchImage();
 };
 
 const fetchImage = async () => {
-    // Fetch image from API and set img url
-    try {
-        const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
-        // Converts to JSON
-        const kittenData = await kittenResponse.json();
-        // console.log(kittenData);
-        const kittenImg = document.querySelector("img");
-        kittenImg.src = kittenData[0].url;
-    } catch (e) {
-        console.log("Failed to fetch image", e);
-    }
+	// Fetch image from API and set img url
+	try {
+		const kittenResponse = await fetch(
+			'https://api.thecatapi.com/v1/images/search?size=small'
+		);
+		// Converts to JSON
+		const kittenData = await kittenResponse.json();
+		// console.log(kittenData);
+		const kittenImg = document.querySelector('img');
+		kittenImg.src = kittenData[0].url;
+	} catch (e) {
+		console.log('Failed to fetch image', e);
+	}
 };
